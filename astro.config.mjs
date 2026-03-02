000df2b62b49fc98b5b2d58b0385d6d2c885cbbf
santiagoarielvz <santiago.ariel@zertify.ai>
2026-03-01 21:25:53 -0300

    feat: update dependencies and add new components
    - Added new dependencies for React, Tailwind CSS, and utility libraries.
    - Updated TypeScript configuration to support React JSX.
    - Introduced a new Button component with variants and sizes using class-variance-authority.
    - Created utility functions for class name merging.
    - Updated global styles to include new CSS variables and themes.
    - Modified the main layout and index page for improved styling and structure.
    - Added a components.json file for shadcn configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });
